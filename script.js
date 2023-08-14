c77901962107c9d01d67f1daeeac270f4cb4fc39
Frannerz <francesstephens@hotmail.com>
2023-08-14 11:27:10 +0100

    updated html

--- script.js
+++ script.js
@@ -311,7 +311,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //favourites messages!
 
-const messageElement=document.getElementById('message');
+const messageElement=document.getElementsByClassName('message');
 
 function addedToFavesMessage() {
     messageElement.textContent = 'Colour added to Faves!';
