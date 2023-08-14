71ada702a08d569f9c044279857e646374dbafa4
Frannerz <francesstephens@hotmail.com>
2023-08-14 08:43:11 +0100

    update favourites and copy functions

--- script.js
+++ script.js
@@ -13,7 +13,11 @@ const lock4 = document.getElementById('lock-icon4');
 const lock5 = document.getElementById('lock-icon5');
 
 //fave icons
-const fave = document.getElementById('heart-icon');
+const fave = document.getElementById('fave1');
+const fave2 = document.getElementById('fave2');
+const fave3 = document.getElementById('fave3');
+const fave4 = document.getElementById('fave4');
+const fave5 = document.getElementById('fave5');
 
 //Colour arrays
 let faveColors = []
@@ -22,6 +26,14 @@ let section2colors = ['8ccef4'];
 let section3colors = ['f5f6a7'];
 let section4colors = ['a7f6c1'];
 let section5colors = ['c8a7f6']
+
+
+//Copy buttons
+const copyButton = document.getElementById("copy")
+const copyButton2 = document.getElementById("copy2")
+const copyButton3 = document.getElementById("copy3")
+const copyButton4 = document.getElementById("copy4")
+const copyButton5 = document.getElementById("copy5")
 
 //generate hex numbers:
 const generateRandomHex=()=>{
@@ -40,7 +52,13 @@ const changeColor = () => {
     const randomColor = generateRandomHex();
     colorSection1.style.backgroundColor = `#${randomColor}`;
     document.getElementById('whichColour1').innerHTML=`#${randomColor}`;
-    section1colors.push(randomColor)
+    section1colors.push(randomColor);
+    fave.src="images/emptyHeart.png";
+    fave2.src="images/emptyHeart.png";
+    fave3.src="images/emptyHeart.png";
+    fave4.src="images/emptyHeart.png";
+    fave5.src="images/emptyHeart.png";
+    removeCopiedImage ()
 }
 
 //change lock
@@ -62,6 +80,7 @@ function changeColor2() {
   const randomColor = generateRandomHex();
   colorSection2.style.backgroundColor = `#${randomColor}`;
   document.getElementById('whichColour2').innerHTML = `#${randomColor}`;
+  section2colors.push(randomColor);
 }
 
 //change lock2
@@ -82,6 +101,7 @@ const changeColor3 = () => {
   const randomColor = generateRandomHex();
   colorSection3.style.backgroundColor = `#${randomColor}`;
   document.getElementById('whichColour3').innerHTML=`#${randomColor}`;
+  section3colors.push(randomColor);
 }
 
 //change lock3
@@ -101,6 +121,7 @@ const changeColor4 = () => {
   const randomColor = generateRandomHex();
   colorSection4.style.backgroundColor = `#${randomColor}`;
   document.getElementById('whichColour4').innerHTML=`#${randomColor}`;
+  section4colors.push(randomColor);
 }
 
 //change lock4
@@ -120,6 +141,7 @@ const changeColor5 = () => {
   const randomColor = generateRandomHex();
   colorSection5.style.backgroundColor = `#${randomColor}`;
   document.getElementById('whichColour5').innerHTML=`#${randomColor}`;
+  section5colors.push(randomColor);
 }
 
 //change lock5
@@ -168,59 +190,234 @@ document.addEventListener('keydown', activateColorChange)
 
 //favourite colors section
 
-//panel
+//side panel function
 function togglePanel() {
   const sidePanel = document.getElementById("sidePanel");
   sidePanel.classList.toggle("open");
 }
 
 
-
-//adding a table
+//adding a table of favourite colours
 const colorTableBody = document.querySelector("#colorTable tbody");
 
 function addColorToTable(index, color) {
     const newRow = document.createElement("tr");
     const colorCell = document.createElement("td");
+    
+    colorCell.style.backgroundColor=`#${color}`
+    colorCell.textContent = `#${color}`
 
-    // indexCell.textContent = index;
-    colorCell.style.backgroundColor = color;
-    // colorCell.style.width="200px";
-    // colorCell.style.width="50px";
-    // colorCell.style.innerHTML=`#${color}`
-
-    // newRow.appendChild(indexCell);
     newRow.appendChild(colorCell);
     colorTableBody.appendChild(newRow);
-
-    colorCell.style.backgroundColor=`#${color}`
 }
 
-//add to faves
+// adding numbers to faves
+
+//add to faves for 1st color
 function addToFaves() {
-  color = section1colors[section1colors.length-1]
-  faveColors.push(color);
-  addColorToTable(faveColors.length, color);
-  return color;
+  
+  color = section1colors[section1colors.length - 1];
+
+  if (!faveColors.includes(color) && faveColors.length<10) {
+    fave.src="images/filledHeart.png";
+    faveColors.push(color);
+    addColorToTable(faveColors.length, color);
+    return color;
+  }
+  return null; 
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   fave.addEventListener('click', addToFaves)
 });
 
+//add to faves for 2nd color
+function addToFaves2() {
+  
+  color = section2colors[section2colors.length - 1];
+
+  if (!faveColors.includes(color)&& faveColors.length<10) {
+    fave2.src="images/filledHeart.png";
+    faveColors.push(color);
+    addColorToTable(faveColors.length, color);
+    return color;
+  }
+  return null; 
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fave2.addEventListener('click', addToFaves2)
+});
+
+//add to faves for 3rd color
+function addToFaves3() {
+  
+  color = section3colors[section3colors.length - 1];
+
+  if (!faveColors.includes(color)&& faveColors.length<10) {
+    fave3.src="images/filledHeart.png";
+    faveColors.push(color);
+    addColorToTable(faveColors.length, color);
+    return color;
+  }
+  return null; 
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fave3.addEventListener('click', addToFaves3)
+});
+
+//add to faves for 4th color
+function addToFaves4() {
+  
+  color = section4colors[section4colors.length - 1];
+
+  if (!faveColors.includes(color)&& faveColors.length<10) {
+    fave4.src="images/filledHeart.png"
+    faveColors.push(color);
+    addColorToTable(faveColors.length, color);
+    return color;
+  }
+  return null; 
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fave4.addEventListener('click', addToFaves4)
+});
+
+//add to faves for 5th color
+function addToFaves5() {
+  
+  color = section5colors[section5colors.length - 1];
+
+  if (!faveColors.includes(color)&& faveColors.length<10) {
+    fave5.src="images/filledHeart.png"
+    faveColors.push(color);
+    addColorToTable(faveColors.length, color);
+  }
+  return null; 
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fave5.addEventListener('click', addToFaves5)
+}); 
+
+
+
+
+//copy to clipboard functions for 1st color
+
+//find the colorCode and initialise copyToClipboard function
+document.getElementById("copy").addEventListener("click", () => {
+  const colorCode = whichColour1.textContent; 
+  copyToClipboard(colorCode);
+  copyButton.src="images/copyFilled.png"
+  copyButton2.src="images/copy.png";
+  copyButton3.src="images/copy.png";
+  copyButton4.src="images/copy.png";
+  copyButton5.src="images/copy.png";
+});
+
+//function to initialise show message
+//.then is a promise, executed if 'writeText' is successful
+//.catch is another promise based method, executed if an error occurs
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      showMessage("Copied to clipboard: " + text);
+    })
+    .catch((error) => {
+      console.error("Clipboard write error:", error);
+    });
+}
+
+const messageElement=document.getElementById('message');
+
+//display the message
+function showMessage(message) {
+  messageElement.textContent = message;
+  messageElement.style.display = "block";
+  setTimeout(() => {
+    messageElement.style.display = "none";
+  }, 1000);
+}
+
+//copy to clipboard function for 2nd color
+//find the colorCode and initialise copyToClipboard function
+document.getElementById("copy2").addEventListener("click", () => {
+  const colorCode2 = whichColour2.textContent; 
+  copyToClipboard(colorCode2);
+  copyButton2.src="images/copyFilled.png"
+  copyButton.src="images/copy.png";
+  copyButton3.src="images/copy.png";
+  copyButton4.src="images/copy.png";
+  copyButton5.src="images/copy.png";
+});
+
+
+//copy to clipboard function for 3rd color
+//find the colorCode and initialise copyToClipboard function
+document.getElementById("copy3").addEventListener("click", () => {
+  const colorCode3 = whichColour3.textContent; 
+  copyToClipboard(colorCode3);
+  copyButton3.src="images/copyFilled.png"
+  copyButton.src="images/copy.png";
+  copyButton2.src="images/copy.png";
+  copyButton4.src="images/copy.png";
+  copyButton5.src="images/copy.png";
+});
+
+//copy to clipboard function for 4th color
+//find the colorCode and initialise copyToClipboard function
+document.getElementById("copy4").addEventListener("click", () => {
+  const colorCode4 = whichColour4.textContent; 
+  copyToClipboard(colorCode4);
+  copyButton4.src="images/copyFilled.png"
+  copyButton.src="images/copy.png";
+  copyButton3.src="images/copy.png";
+  copyButton2.src="images/copy.png";
+  copyButton5.src="images/copy.png";
+});
+
+//copy to clipboard function for 4th color
+//find the colorCode and initialise copyToClipboard function
+document.getElementById("copy5").addEventListener("click", () => {
+  const colorCode5 = whichColour5.textContent; 
+  copyToClipboard(colorCode5);
+  copyButton5.src="images/copyFilled.png"
+  copyButton.src="images/copy.png";
+  copyButton3.src="images/copy.png";
+  copyButton4.src="images/copy.png";
+  copyButton2.src="images/copy.png";
+});
+
+function removeCopiedImage (){
+  copyButton.src="images/copy.png"
+  copyButton2.src="images/copy.png";
+  copyButton3.src="images/copy.png";
+  copyButton4.src="images/copy.png";
+  copyButton5.src="images/copy.png";
+}
+
+
+// //refactored code for faves not working
+// function addToFaves(colorIndex, colorsArray, faveElement) {
+//   faveElement.src = "images/filledHeart.png";
+//   color = colorsArray[colorsArray.length - 1];
+
+//   if (!faveColors.includes(color)) {
+//     faveColors.push(color);
+//     addColorToTable(faveColors.length, color);
+//     return color;
+//   }
+//   return null;
+// }
+
 // document.addEventListener("DOMContentLoaded", function () {
-//   const addColorButton = document.querySelector("#addColorButton");
-
-//   addColorButton.addEventListener("click", () => {
-//       const newColor = prompt("Enter a color (e.g. red, blue, #FF9900):");
-//       if (newColor) {
-//           addToFaves(newColor); // Call your function to add color to faves
-//       }
-//   });
+//   for (let i = 1; i <= 5; i++) {
+//     const faveElement = document.getElementById(`fave${i}`);
+//     const colorsArray = window[`section${i}colors`]; 
+//     faveElement.addEventListener('click', () => addToFaves(i, colorsArray, faveElement));
+//   }
 // });
-
-
-
-
-
 
